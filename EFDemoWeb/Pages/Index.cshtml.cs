b97976d0c12d7123dcfda15c58279b08ac9a78da
Michael Morris <mpf.morris@gmail.com>
2020-04-17 14:16:00 +1200

    First and Final commit

---
+++ EFDemoWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using EFDataAccessLibrary.DataAccess;
+using EFDataAccessLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+// Benefits of Entity Framework Core
+// 1. Faster development speed
+// 2. You don 't have to know SQL
+
+// Benefits of Dapper
+// 1.Faster in production
+// 2. Easier to work with for SQL developer
+// 3. Designed for loose coupling
+
+
+namespace EFDemoWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly PeopleContext _db;
+
+        public IndexModel(ILogger<IndexModel> logger, PeopleContext db)
+        {
+            _logger = logger;
+            this._db = db;
+        }
+
+        public void OnGet()
+        {
+            LoadSampleData();
+
+            // Innificient
+            var people = _db.People
+                .Include(a => a.Addresses)
+                .Include(e => e.EmailAddresses)
+                //.Where(x => ApprovedAge(x.Age)); 
+                .Where(x => x.Age >= 18 && x.Age <= 65)
+                .ToList();
+        }
+
+        private bool ApprovedAge(int age)
+        {
+            return (age >= 18 && age <= 65); //Innificient
+        }
+
+        private void LoadSampleData()
+        {
+            if (_db.People.Count() == 0)
+            {
+                string file = System.IO.File.ReadAllText("generated.json");
+                var people = JsonSerializer.Deserialize<List<Person>>(file);
+                _db.AddRange(people);
+                _db.SaveChanges();
+            }
+        }
+    }
+}
